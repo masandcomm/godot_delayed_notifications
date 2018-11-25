cf38529b9517d6561c5f2199d2f20030bc8eb4cf
bobanlukic <bbnlkc@gmail.com>
2018-11-25 17:02:01 +0100

    Minor change

--- android/GodotDelayedNotifications.java
+++ android/GodotDelayedNotifications.java
@@ -8,19 +8,13 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 
+import org.godotengine.godot.GodotDelayedNotificationReceiver;
+
 import com.godot.game.R;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Iterator;
 
 public class GodotDelayedNotifications extends Godot.SingletonBase {
     private final int TO_MILLIS = 1000;
