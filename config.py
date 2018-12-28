60bf1c8e62123f6ffb4ed18e50a6ccaa84dbf532
bobanlukic <bbnlkc@gmail.com>
2018-12-28 08:23:56 +0100

    Support changed to 28

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ def can_build(env, platform):
 
 def configure(env):
     if env["platform"] == "android":
-        env.android_add_dependency("implementation 'com.android.support:appcompat-v7:27.0.0'")
+        env.android_add_dependency("implementation 'com.android.support:appcompat-v7:28.0.0'")
         env.android_add_java_dir("android")
         env.android_add_to_manifest("android/AndroidManifestChunk.xml")
         env.android_add_default_config("minSdkVersion 15")
